2b574d80ef46cd66e6602f0cafd225c422220252
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-03-22 20:09:06 +0100

    losovani na tlacitko

--- sportka.js
+++ sportka.js
@@ -5,20 +5,30 @@ let vyherniCisla = document.querySelector('#vyherni-cisla');
 let tazenaCisla = [];
 
 //naplním osudí
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
-}
+
 
 // z osudi vyberu 7 náhodných čísel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
+function losuj(){
+  tazenaCisla = [];
+  vyherniCisla.innerHTML = '';
+  osudi = [];
 
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
+  for (let i = 1; i <= 49; i = i + 1) {
+    osudi.push(i);
+    }; 
+  
+  for (let i = 0; i < 7; i++) {
+      let vyherniIndex = Math.floor(Math.random() * osudi.length);
+      let vyherniCislo = osudi[vyherniIndex];
+
+      tazenaCisla.push(vyherniCislo);
+      osudi.splice(vyherniIndex, 1);
+    };
+
+    for (let i = 0; i < tazenaCisla.length; i++) {
+      vyherniCisla = document.querySelector('#vyherni-cisla');
+      vyherniCisla.innerHTML += '<span class="cislo">' +  tazenaCisla[i] + '</span>';
+      
+    }
 }
-
 //vypíšu do HTML
-for (let i = 0; i < tazenaCisla.length; i++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' +  tazenaCisla[i] + '</span>';
-}
